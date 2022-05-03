5424ff819ba5cf6bfb0ac2aaa41d8f0c01d2a2fc
Jonathan Hew <jonathanhew@Jonathans-MacBook-Pro.local>
2022-05-03 21:24:16 +0100

    add new contact functionality now works but doesnt save

--- contact-app/src/components/AddContact.js
+++ contact-app/src/components/AddContact.js
@@ -5,16 +5,19 @@ class AddContact extends React.Component
     state = {
         name: "",
         email: ""
-    }
+    };
 
     add = (e) => {
         e.preventDefault();
         if (this.state.name === "" && this.state.email === "") {
-            alert("All the fields are mandatory! ")
+            alert("All the fields are mandatory! ");
+            return;
         }
 
-        console.log(this.state);
-    }
+        this.props.addContactHandler(this.state);
+        //clears the input fields once submitted
+        this.setState({name:"", email:""});
+    };
 
     render () {
         return (
@@ -23,11 +26,23 @@ class AddContact extends React.Component
                 <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                         <label>Name</label>
-                        <input type="text" name="name" placeholder="Name" value={this.state.name} onChange={(e) => this.setState({name: e.target.value})}/>
+                        <input 
+                            type="text" 
+                            name="name" 
+                            placeholder="Name" 
+                            value={this.state.name} 
+                            onChange={(e) => this.setState({name: e.target.value})}
+                        />
                     </div>
                     <div className="field">
                         <label>Email</label> 
-                        <input type="text" name="email" placeholder="Email" value={this.state.email} onChange={(e) => this.setState({email: e.target.value})}/>
+                        <input 
+                            type="text" 
+                            name="email" 
+                            placeholder="Email" 
+                            value={this.state.email} 
+                            onChange={(e) => this.setState({email: e.target.value})}
+                        />
                     </div>
                     <button className="ui button blue">Add</button>
                 </form>
