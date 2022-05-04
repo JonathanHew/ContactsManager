791d1bee630cedcabdad530c2adc3a77a16bf059
Jonathan Hew <jonathanh9655@hotmail.com>
2022-05-04 21:07:19 +0100

    github test

--- contact-app/src/components/ContactList.js
+++ contact-app/src/components/ContactList.js
@@ -6,7 +6,7 @@ const ContactList = (props) => {
 
     const renderContactList = props.contacts.map((contact) => {
         return (
-            <ContactCard contact={contact}/ >
+            <ContactCard contact={contact}/>
         );
     });
     return <div className="ui celled list">{renderContactList}</div>;
